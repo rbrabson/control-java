b78afc8a8fb2e2514c40242d30bfa8aca1c54d05
Roy Brabson <brabson@alumni.duke.edu>
2026-03-11 16:57:03 -0400

    jitpack compile issues

--- src/test/java/com/rbrabson/control/feedback/FullStateFeedbackTest.java
+++ src/test/java/com/rbrabson/control/feedback/FullStateFeedbackTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class FullStateFeedbackTest {
     @Test
     void calculatesDotProductOfErrorAndGain() {
-        FullStateFeedback fsf = new FullStateFeedback(new double[] { 1.5, 0.3 });
+        final FullStateFeedback fsf = new FullStateFeedback(new double[] { 1.5, 0.3 });
         double out = fsf.calculate(new double[] { 10.0, 0.0 }, new double[] { 8.0, 1.0 });
         assertEquals(2.7, out, 1e-9);
     }
 
     @Test
     void throwsOnMismatchedVectorLengths() {
-        FullStateFeedback fsf = new FullStateFeedback(new double[] { 1.0, 1.0 });
+        final FullStateFeedback fsf = new FullStateFeedback(new double[] { 1.0, 1.0 });
         assertThrows(IllegalArgumentException.class, new org.junit.jupiter.api.function.Executable() {
             @Override
             public void execute() throws Throwable {
