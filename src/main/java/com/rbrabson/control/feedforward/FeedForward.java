01b4ad1cc5bf6c02b95d6fa40d913278fcecbbe8
Roy Brabson <brabson@alumni.duke.edu>
2026-03-25 21:24:49 -0400

    code cleanup

--- src/main/java/com/rbrabson/control/feedforward/FeedForward.java
+++ src/main/java/com/rbrabson/control/feedforward/FeedForward.java
@@ -11,7 +11,6 @@ public class FeedForward {
     private final double kS;
     private final double kV;
     private final double kA;
-    private double kG;
     private double kCos;
 
     /**
@@ -28,22 +27,7 @@ public class FeedForward {
         this.kS = kS;
         this.kV = kV;
         this.kA = kA;
-        this.kG = 0.0;
         this.kCos = 0.0;
-    }
-
-    /**
-     * Creates a copy of this FeedForward controller with a gravity gain set. This
-     * gain can be used to compensate for the effect of gravity on the system, such
-     * as when controlling an arm or elevator.
-     *
-     * @param kG The gravity gain, representing the output required to compensate
-     *           for gravity.
-     * @return A new FeedForward controller with the gravity gain set.
-     */
-    public FeedForward withGravityGain(double kG) {
-        this.kG = kG;
-        return this;
     }
 
     /**
@@ -79,7 +63,9 @@ public class FeedForward {
      *         for static friction, gravity, and position effects as configured.
      */
     public double calculate(double position, double velocity, double acceleration) {
-        double output = kV * velocity + kA * acceleration + kG;
+        double output = kS;
+        output += kV * velocity;
+        output += kA * acceleration;
         if (kCos != 0.0) {
             output += kCos * Math.cos(position);
         }
